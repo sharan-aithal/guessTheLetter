616669a3c7216a4e03ddb98cdc80acfa8aaa382d
Sharan Aithal <sharanaithal0@yahoo.com>
2021-01-16 13:31:34 +0530

    Added random integer generator and checking input charterer is between a and z.

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
-	fmt.Print("Enter a letter : ")
+	// get random number from "a" to "z"
+	num := getRandomInt(int('a'), int('z'))
+	fmt.Print("Enter a alphabet character : ")
 	reader := bufio.NewReader(os.Stdin)
 	line, _, err := reader.ReadLine()
 	if err != nil {
@@ -15,12 +19,22 @@ func main() {
 	}
 	if len(line) == 1 {
 		// print line[0]
-		fmt.Println(line[0])
+		fmt.Println("Your alphabet is :", string(line[0]))
+		if int(line[0]) >= int('a') && int(line[0]) <= int('z') {
+			fmt.Println("Entered letter is in the range")
+		} else {
+			fmt.Println("Enter only small letter alphabet character (a-z)")
+		}
 	} else if len(line) == 0 {
 		// print no line
-		fmt.Println("Enter any alphabet.")
+		fmt.Println("Enter any alphabet character.")
 	} else {
 		// print line is large
 		fmt.Println("Enter only single alphabet.")
 	}
+}
+
+func getRandomInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min) + min
 }
