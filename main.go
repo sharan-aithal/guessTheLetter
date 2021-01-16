b5da04c4fb4218135c1d17e1fb9c73dfcb06b680
Sharan Aithal <sharanaithal0@yahoo.com>
2021-01-16 15:20:30 +0530

    Added loop for asking user to enter correct letter.

--- main.go
+++ main.go
@@ -11,26 +11,45 @@ import (
 func main() {
 	// get random number from "a" to "z"
 	num := getRandomInt(int('a'), int('z'))
-	fmt.Print("Enter a alphabet character : ")
-	reader := bufio.NewReader(os.Stdin)
-	line, _, err := reader.ReadLine()
-	if err != nil {
-		fmt.Println(err)
-	}
-	if len(line) == 1 {
-		// print line[0]
-		fmt.Println("Your alphabet is :", string(line[0]))
-		if int(line[0]) >= int('a') && int(line[0]) <= int('z') {
-			fmt.Println("Entered letter is in the range")
-		} else {
-			fmt.Println("Enter only small letter alphabet character (a-z)")
+	found := false
+
+	for i := 0; i < 5; i++ {
+		fmt.Print(string(rune(num)), i, " Enter a alphabet character : ")
+		reader := bufio.NewReader(os.Stdin)
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			fmt.Println(err)
 		}
-	} else if len(line) == 0 {
-		// print no line
-		fmt.Println("Enter any alphabet character.")
-	} else {
-		// print line is large
-		fmt.Println("Enter only single alphabet.")
+
+		if len(line) == 1 {
+			if int(line[0]) >= int('a') && int(line[0]) <= int('z') {
+				if int(line[0]) == num {
+					fmt.Println("Yeah!!! You guessed that letter correctly.")
+					found = true // user already guessed the answer
+					break
+				} else if int(line[0]) > num {
+					fmt.Println("Your guess is too high.")
+				} else if int(line[0]) < num {
+					fmt.Println("Your guess is too low.")
+				}
+			} else {
+				// if user entered other than letters from a to z
+				fmt.Println("Enter only small letter alphabet character (a-z).")
+				continue
+			}
+		} else if len(line) == 0 {
+			// if user not provided input or given empty
+			fmt.Println("Enter any alphabet character.")
+			continue // continue to ask input
+		} else {
+			// if user entered more than one letter
+			fmt.Println("Enter only single alphabet.")
+			continue // continue asking user input
+		}
+	}
+	// check user guess
+	if !found {
+		fmt.Println("You are not guess it. The actual letter is \"" + string(rune(num)) + "\".")
 	}
 }
 
